564eb0c8c9076156839cb23b304912abaf423353
sai <sai9@gmail.com>
2021-09-24 15:19:02 +0530

    first

---
+++ src/main/java/com/exampleaws/aws/beanstack/AwsBeanstackApplication.java
@@ -0,0 +1,13 @@
+package com.exampleaws.aws.beanstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBeanstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBeanstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampleaws/aws/beanstack/AwsBeanstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleaws.aws.beanstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBeanstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
